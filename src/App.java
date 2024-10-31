d1c7799697cb77c6aefc4b89a3b03624bd413ec6
allisonfr <157404762+allisonfr@users.noreply.github.com>
2024-10-31 13:07:42 -0400

    Hi, I added some notes that I am confused on. Thank you. Alli

--- src/App.java
+++ src/App.java
@@ -2,14 +2,44 @@ import java.util.*;
 
 public class App {
 
-    //Write code that will:
-    //1. Store the following values in an ArrayList: "Java", "Python", "JavaScript"
-    //   The name of the ArrayList is up to you.
-    //2. Print the contents of your ArrayList using loops. This should work even if 
-    //   I add more elements to your ArrayList. For example, I might add "Ruby" to the
-    //   ArrayList and this should still work.
-    public static void main(String[] args) throws Exception {
+    // Write code that will:
+    // 1. Store the following values in an ArrayList: "Java", "Python", "JavaScript"
+    // The name of the ArrayList is up to you.
+    // 2. Print the contents of your ArrayList using loops. This should work even if
+    // I add more elements to your ArrayList. For example, I might add "Ruby" to the
+    // ArrayList and this should still work.
+
+
+   
+    public static void main(String[] args) {
+     int Java =0;
+     int Python =0;
+     int JavaScript =0;
+
+
+    // import the list so the program can use it
+  
+        // create a list
+        ArrayList<String> list = new ArrayList<>();
+
+        { // the list isn't used yet
+            ArrayList<Java> list = new ArrayList<>();
+         //DO I NEED LIST ADD HERE?
+        
+         ArrayList<Python> list = new ArrayList<>();
+             //DO I NEED LIST ADD HERE?
     
+            ArrayList<JavaScript> list = new ArrayList<>();
+             //DO I NEED LIST ADD HERE?
+}
+
+  
+
+        for (int index = 0; index < ArrayList.size(); index++) {
+            System.out.println(String.get(index));
+
+        }
 
     }
 }
+
